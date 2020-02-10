623ab77ea1d85d52737d0ebc12c2ada34a88dac4
Ryan Larkin <rlarkin212@gmail.com>
2020-02-10 20:39:06 +0000

    server setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate log;
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use listenfd::ListenFd;
+use std::env;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    env_logger::init();
+
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| App::new().service(index));
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = env::var("HOST").expect("host not set");
+            let port = env::var("PORT").expect("port not set");
+            server.bind(format!("{}:{}", host, port))?
+        }
+    };
+
+    info!("starting server");
+    server.run().await
 }
